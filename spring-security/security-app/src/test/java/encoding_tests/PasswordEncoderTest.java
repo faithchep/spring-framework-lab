567b881630aa13adf9d934d8255b94be5672b72a
samuelowino <samuelowino43@yahoo.com>
2022-12-24 14:14:32 +0300

    Password Encoding

---
+++ spring-security/security-app/src/test/java/encoding_tests/PasswordEncoderTest.java
@@ -0,0 +1,24 @@
+package encoding_tests;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+
+public class PasswordEncoderTest {
+
+    public static final Logger LOGGER = LoggerFactory.getLogger(PasswordEncoderTest.class);
+
+    @Test
+    public void shouldEncodePasswordTest() {
+        String plainPassword = "1234567Password"; //not recommended password
+        var encoder = PasswordEncoderFactories.createDelegatingPasswordEncoder();
+        var encodedPassword = encoder.encode(plainPassword);
+
+        LOGGER.info("Plain password: " + plainPassword + " encoded password " + encodedPassword);
+
+        Assertions.assertThat(encodedPassword).isNotNull();
+        Assertions.assertThat(encodedPassword).isNotEqualTo(plainPassword);
+    }
+}
